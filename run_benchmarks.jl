12cd914da5a47cab73918bf5dab67c45d4d0f2c4
Romeo Valentin <romeov@stanford.edu>
2023-05-19 13:01:41 -0700

    Fix benchmarks for comparison to pytorch
    Result: In limited experiments, Flux about 50% slower than pytorch (I
    can live with that).

--- run_benchmarks.jl
+++ run_benchmarks.jl
@@ -1,23 +1,31 @@
 using Revise
+using BenchmarkTools
+using ProgressBars
+import Zygote: withgradient
+using Flux
+import Random: seed!
 includet("own_mae.jl")
-imgs = rand(Float32, 224, 224, 3, 7);
-results = []
-grads = []
-for bs in [nothing, 7]
-  seed!(1)
-  m = make_model();
-  m(copy(imgs));
-  s, gs = withgradient(Flux.params(m)) do
-    m(imgs) |> sum  # check the gradients...
-  end;
-  push!(results, s)
-  push!(grads, gs)
+
+begin
+  imgs = rand(Float32, 224, 224, 3, 7);
+  results = []
+  grads = []
+  for bs in [nothing, 7]
+    seed!(1)
+    m = make_model();
+    m(copy(imgs));
+    s, gs = withgradient(Flux.params(m)) do
+      m(imgs) |> sum  # check the gradients...
+    end;
+    push!(results, s)
+    push!(grads, gs)
+  end
+  @assert results[1] ≈ results[2]
+  @assert length(grads[1].params) == length(grads[2].params)
+  lhs = sum(sum(v) for v in values(grads[1].grads) if v isa Array);
+  rhs = sum(sum(v) for v in values(grads[2].grads) if v isa Array);
+  # @assert (lhs ≈ rhs) "The gradients are not equal! See $lhs != $rhs"
 end
-@assert results[1] ≈ results[2]
-@assert length(grads[1].params) == length(grads[2].params)
-lhs = sum(sum(v) for v in values(grads[1].grads) if v isa Array);
-rhs = sum(sum(v) for v in values(grads[2].grads) if v isa Array);
-# @assert (lhs ≈ rhs) "The gradients are not equal! See $lhs != $rhs"
 
 @info "Starting benchmarks"
 benchmarks = Dict(
@@ -33,8 +41,8 @@ benchmarks = Dict(
                 end
     )
     for bs  in [nothing, 16, 32],
-        dev in [gpu]
-        # dev in [cpu, gpu]
+        dev in [cpu, gpu]
+        # dev in [gpu]
 )  # conclusion: preallocating is about 10-20% faster
 
 for bs in [nothing, 7]
